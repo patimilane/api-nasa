61a8aa1e054446745397f10cbd9ea57c492b4d9f
patriciamilane <patriciamilano96@gmail.com>
2022-02-23 13:31:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# apod-nasa
+
+## https://patriciamilane.github.io/api-nasa/
+
+Página Inicial: 
+
+![tela inicial completa](https://github.com/patriciamilane/api-nasa/blob/master/img/1.png)
+
+
+Página de pesquisa:
+
+![tela pesquisa](https://github.com/patriciamilane/api-nasa/blob/master/img/2.png)
+
+
+Página de pesquisa com Dark Mode:
+
+![tela pesquisa](https://github.com/patriciamilane/api-nasa/blob/master/img/3.png)
+
---
+++ script.js
@@ -0,0 +1,55 @@
+function inicio() {
+    location = "index.html";
+}
+
+function pesquisar() {
+    location = "pesquisar.html";
+}
+
+function pesquisarData() {
+    let data = $(".data").val();
+
+    $.ajax({ url: `https://api.nasa.gov/planetary/apod?api_key=Sk0oacheSAeMtk6f8altpm2U0Y6OHOmxlOU4ncTU&date=${data}`,
+        success: function (resultado) {
+            resultadoPesquisa(resultado);
+            $(".mensagemErro").html(" "); //pra sumir o erro quando a requisição funcionar
+        },
+        error: function () {
+            $(".mensagemErro").html("Data inválida").css({color: "red"});
+        }
+    });
+}
+
+function resultadoPesquisa(pesquisa) {
+    const imagem = $("#imagem");
+    const titulo = $("#titulo");
+    const autor = $("#autor");
+    const legenda = $("#legenda");
+    //const data = $("#data");
+
+    //data.html(moment(pesquisa.date).format('DD MMMM, YYYY'));
+    titulo.html(pesquisa.title);
+    autor.html(`Créditos: ${pesquisa.copyright}`);
+    legenda.html(`Sobre: ${pesquisa.explanation}`);
+
+    if (pesquisa.media_type == 'image') {
+        imagem.html(`<img id="midiaJS" src="${pesquisa.url}"/>`);
+    } else {
+        imagem.html(`<iframe id="midiaJS" src="${pesquisa.url}?autoplay=1&mute=1" width="650" height="375"></iframe>`);
+    }
+}
+
+/////////////// "DARK MODE"
+
+const selecionar = document.getElementById('selecionar');
+const divinputs = document.getElementById('divinputs');
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+
+selecionar.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+    divinputs.style.color = "#4D4F50";    
+    header.style.filter = "invert(62%) sepia(19%) saturate(26%) hue-rotate(86deg) brightness(78%) contrast(119%)";
+    footer.style.filter = "invert(62%) sepia(19%) saturate(26%) hue-rotate(86deg) brightness(68%) contrast(119%)";
+});
+
